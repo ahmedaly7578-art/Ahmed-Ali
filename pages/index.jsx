16fe4b5a7d73dc1f601c580898b7f5b7c4593b1c
adsspecialistwarqa7-alt <adsspecialist.warqa7@gmail.com>
2025-12-15 14:58:24 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,12 @@
 import { motion } from "framer-motion";
-
 import ParticlesContainer from "../components/ParticlesContainer";
 import ProjectsBtn from "../components/ProjectsBtn";
 import Avatar from "../components/Avatar";
-
 import { fadeIn } from "../variants";
 
 const Home = () => {
   return (
-    <div className="bg-primary/60 h-full">
+    <div className="bg-primary/60 h-full relative">
       {/* text */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
@@ -32,7 +30,7 @@ const Home = () => {
             exit="hidden"
             className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
           >
-            ahmed ipsum dolor sit amet consectetur, adipisicing elit. Voluptate,
+            AM ipsum dolor sit amet consectetur, adipisicing elit. Voluptate,
             exercitationem harum, quia nulla temporibus deleniti libero veniam
             vero beatae numquam ducimus illum ab similique ipsam tempore fugit
             quod laudantium debitis.
@@ -53,6 +51,7 @@ const Home = () => {
           </motion.div>
         </div>
       </div>
+
       {/* image */}
       <div className="w-[1280px] h-full absolute right-0 bottom-0">
         {/* bg img */}
@@ -65,19 +64,19 @@ const Home = () => {
         {/* particles */}
         <ParticlesContainer />
 
-        {/* avatar */}
+        {/* avatar on desktop */}
         <motion.div
           variants={fadeIn("up", 0.5)}
           initial="hidden"
           animate="show"
           exit="hidden"
           transition={{ duration: 1, ease: "easeInOut" }}
-          className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%] xl:hidden"
+          className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%] xl:flex hidden"
         >
           <Avatar />
         </motion.div>
 
-        {/* avatar (mobile, top half) */}
+        {/* avatar on mobile (top half) */}
         <motion.div
           variants={fadeIn("up", 0.5)}
           initial="hidden"
