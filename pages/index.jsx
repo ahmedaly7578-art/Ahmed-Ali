ffff449f77c5c000308f4ed16016a5fae4f1057e
adsspecialistwarqa7-alt <adsspecialist.warqa7@gmail.com>
2025-12-17 13:51:02 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,7 +19,10 @@ const Home = () => {
             exit="hidden"
             className="mb-6 text-center"
           >
-        <p className="mb-4">Transforming Ideas</p>
+            <h2 className="text-white font-bold text-xl"></h2>
+          </motion.div>
+
+          {/* Icons */}
           <motion.div
             variants={fadeIn("down", 0.2)}
             initial="hidden"
@@ -61,7 +64,7 @@ const Home = () => {
             exit="hidden"
             className="text-sm text-center mb-8 text-gray-300 leading-relaxed"
           >
-            ss da dolor sit amet consectetur, adipisicing elit. Voluptate,
+            aa ipsum dolor sit amet consectetur, adipisicing elit. Voluptate,
             exercitationem harum, quia nulla temporibus deleniti libero veniam
             vero beatae numquam ducimus illum ab similique ipsam tempore fugit
             quod laudantium debitis.
@@ -80,72 +83,65 @@ const Home = () => {
       </div>
 
       {/* Desktop Layout */}
-      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+      <div className="hidden xl:block w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto px-4">
           {/* title */}
           <motion.h1
             variants={fadeIn("down", 0.2)}
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="h1"
+            className="h1 text-6xl font-bold mb-6"
           >
             Transforming Ideas <br /> Into{" "}
             <span className="text-accent">Digital Reality</span>
           </motion.h1>
-
           {/* subtitle */}
           <motion.p
             variants={fadeIn("down", 0.3)}
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+            className="max-w-xl mb-16 text-lg"
           >
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate,
+            Ha ipsum dolor sit amet consectetur, adipisicing elit. Voluptate,
             exercitationem harum, quia nulla temporibus deleniti libero veniam
             vero beatae numquam ducimus illum ab similique ipsam tempore fugit
             quod laudantium debitis.
           </motion.p>
-
           {/* btn */}
-          <div className="flex justify-center xl:hidden relative">
-            <ProjectsBtn />
-          </div>
           <motion.div
             variants={fadeIn("down", 0.4)}
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="hidden xl:flex"
           >
             <ProjectsBtn />
           </motion.div>
         </div>
-      </div>
-      {/* image */}
-      <div className="w-[1280px] h-full absolute right-0 bottom-0">
-        {/* bg img */}
-        <div
-          role="img"
-          className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"
-          aria-hidden
-        />
 
-        {/* particles */}
-        <ParticlesContainer />
-
-        {/* avatar */}
-        <motion.div
-          variants={fadeIn("up", 0.5)}
-          initial="hidden"
-          animate="show"
-          exit="hidden"
-          transition={{ duration: 1, ease: "easeInOut" }}
-          className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]"
-        >
-          <Avatar />
-        </motion.div>
+        {/* image */}
+        <div className="w-full h-full xl:w-[1280px] xl:h-full absolute right-0 bottom-0">
+          {/* bg img */}
+          <div
+            role="img"
+            className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"
+            aria-hidden
+          />
+          {/* particles */}
+          <ParticlesContainer />
+          {/* avatar on desktop */}
+          <motion.div
+            variants={fadeIn("up", 0.5)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            transition={{ duration: 1, ease: "easeInOut" }}
+            className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%] mb-16"
+          >
+            <Avatar />
+          </motion.div>
+        </div>
       </div>
     </div>
   );
